fde61f695a24e4d0b74ce9c3e5336631d8c18dba
paha_koha <pashagerasimik@mail.ru>
2023-11-05 18:47:27 +0300

    Test Task9

---
+++ src/Task9_Geasimik_Pavel/Comments.java
@@ -0,0 +1,14 @@
+package Task9_Geasimik_Pavel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comments {
+    public static List<String> commentsList = new ArrayList<>();
+
+    static {
+        commentsList.add("Ты совсем дурак?");
+        commentsList.add("Ты - дуралей!");
+        commentsList.add("Если бы я знал, какой ты пройдоха, никогда бы с тобой не общался!");
+    }
+}
